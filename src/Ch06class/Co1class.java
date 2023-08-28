a3c488d42a76e7e8326eb0fa9108182c63d87e44
dasom <zzzoo42@naver.com>
2023-08-28 15:33:10 +0900

    객체와 클래스

---
+++ src/Ch06class/Co1class.java
@@ -0,0 +1,27 @@
+package Ch06class;
+
+public class Co1class {
+    public static void main(String[] args) {
+        MyClass01 o1 =  new MyClass01();
+        System.out.println("System.identityHashCode(o1) =" + System.identityHashCode(o1));
+
+        System.out.println("o1.age = " + o1.age);
+
+        o1.age = 30;
+        System.out.println("o1.age = " + o1.age);
+
+        o1.age = 60;
+
+        System.out.println("o2.age = " +o1.age);
+    }
+}
+
+class MyClass01{
+    //instance fields
+    //인스터스가 가져야하는 속성들
+    int age;
+
+
+    //instance methods
+    //인스턴스가 가져야하는 기능들
+}
---
+++ src/ch05reference/C12instance.java
@@ -0,0 +1,12 @@
+package ch05reference;
+
+public class C12instance {
+
+    public static void main(String[] args) {
+        // instance 인스턴스
+        // object  오브젝트
+        // 객체 :
+        // 1. 속성(값,필드,property,attribute)
+        // 2. 기능(메소드,함수,액션)
+    }
+}
