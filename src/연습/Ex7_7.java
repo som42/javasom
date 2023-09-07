9a36173aa447f506b60fd6a6c9c8384dd05a5a55
dasom <zzzoo42@naver.com>
2023-09-07 17:21:18 +0900

    9월7일 제네릭 컬렉션 list, set,Map

--- src/연습/Ex7_7.java
+++ src/연습/Ex7_7.java
@@ -3,7 +3,10 @@ package 연습;
 import java.lang.module.FindException;
 
 public class Ex7_7 {
-    public static void main(String[] args) {
-
+    public int[] solution(int[] numbers){
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i]* =2;
+        }
+        return numbers;
     }
 }
